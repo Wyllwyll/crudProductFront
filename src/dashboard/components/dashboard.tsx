3d46553702789201046f2a9ac876dc2d247da7a0
Wyllwyll <terry.boeuf@hotmail.fr>
2023-03-21 15:15:59 +0100

    fix/feat: ajout fonction modification et suppression de produits

--- src/dashboard/components/dashboard.tsx
+++ src/dashboard/components/dashboard.tsx
@@ -1,35 +1,93 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { TProductFull } from "../types/TProductFull";
+import OneProduct from "./product";
 
 export default function Dashboard() {
+
     const [allProducts, setAllProducts] = useState<TProductFull[]>([])
+    const [addForm, setAddForm] = useState(false);
+    const nameRef = useRef<HTMLInputElement>(null);
+    const priceRef = useRef<HTMLInputElement>(null);
+    const quantityRef = useRef<HTMLInputElement>(null);
 
 
+
+    /**
+     * ajout d'un produit
+     */
+    const handleSubmitAdd = () => {
+
+        if (nameRef.current && priceRef.current && quantityRef.current) {
+
+            const body = JSON.stringify({
+                name: nameRef.current.value,
+                price: parseInt(priceRef.current.value),
+                quantity: parseInt(quantityRef.current.value),
+            });
+
+            const options = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: body,
+            };
+
+            fetch('http://localhost:8000/api/products', options)
+                .then(response => response.json())
+                .then(response => {
+
+                    if (response.statusCode === 201) {
+                        const productsCopy = [...allProducts];
+                        productsCopy.push(response.data);
+                        setAllProducts(productsCopy);
+                        setAddForm(false);
+                    }
+                    else {
+                        console.log(response.error);
+                    };
+                })
+        }
+    };
+
+
+    const handleDeleteProduct = (productId: any) => {
+
+        setAllProducts(allProducts.filter((product: TProductFull) => product.id !== productId));
+    }
+
+
+
+    const handleUpdate = (data: TProductFull) => {
+        const updatedProducts = allProducts.map((product: TProductFull) => {
+
+            if (product.id === data.id) {
+                return data;
+            };
+
+            return product;
+        });
+        setAllProducts(updatedProducts)
+    }
+
+
+    /**
+     * recupere et affiche toutes les données
+     */
     useEffect(() => {
-
         fetch('http://localhost:8000/api/products')
             .then((response) =>
                 response.json())
-
             .then((data) => {
-                console.log(data);
+                // console.log(data);
                 setAllProducts(data.data)
-
             })
     }, []
     )
-    const returnProducts = allProducts.map((elm, i) => (
-        <tr key={i}>
-            <td>{elm.id}</td>
-            <td>{elm.name}</td>
-            <td>{elm.price}</td>
-            <td>{elm.quantity}</td>
-        </tr>
-    ))
-
-
+    const returnProducts = allProducts.map((product, i) =>
+        <OneProduct product={product} update={handleUpdate} handleDeleteProduct={handleDeleteProduct} ></OneProduct>
+    )
 
     return (
+
         <>
             <div className="d-flex flex-wrap bg-primary height height-header fs-3">
                 <p>
@@ -37,7 +95,57 @@ export default function Dashboard() {
                 </p>
             </div>
 
+            <div className="ms-5 me-5">
+                {addForm ?
+                    <form className="row g-3 mt-4  mb-4   border" onSubmit={handleSubmitAdd} >
 
+                        <div className="">
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Nom</label>
+                            <input
+                                type="name"
+                                className="form-control"
+                                id="nameControl"
+                                ref={nameRef}
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Prix</label>
+                            <input
+                                type="price"
+                                className="form-control"
+                                id="priceControl"
+                                ref={priceRef}
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="exampleFormControlInput1" className="form-label">Quantité</label>
+                            <input
+                                type="quantity"
+                                className="form-control"
+                                id="quantityControl"
+                                ref={quantityRef}
+                                required
+                            />
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="btn btn-primary mb-3"
+                        >
+                            Ajouter un produit
+                        </button>
+
+                    </form>
+
+                    : <button type="button" className="btn btn-primary mt-4 ms-2 mb-4" onClick={() => setAddForm(true)}>Ajouter un produit</button>
+
+                }
+
+            </div>
             <table className="table">
                 <thead>
                     <tr >
@@ -49,9 +157,7 @@ export default function Dashboard() {
                     </tr>
                 </thead>
                 <tbody>
-
                     {returnProducts}
-
                 </tbody>
             </table>
 
---
+++ src/dashboard/components/product.tsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import { TProductFull } from "../types/TProductFull";
+
+export default function OneProduct(props: {
+    handleDeleteProduct: (productId: number) => void,
+    product: TProductFull,
+    update: (data: TProductFull) => void
+
+}) {
+
+    const [isEditing, setIsEditing] = useState(false)
+    const [newProductName, setNewProductName] = useState<string>("");
+    const [newProductPrice, setNewProductPrice] = useState<string>("");
+    const [newProductQuantity, setNewProductQuantity] = useState<string>("");
+    const [editingProductId, setEditingProductId] = useState<number>(0);
+
+
+
+
+    const handleUpdateProduct = (productId: number, name: string, price: string, quantity: string) => {
+
+        const body = JSON.stringify({
+            name: name,
+            price: parseInt(price),
+            quantity: parseInt(quantity),
+        });
+
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: body,
+        };
+
+        fetch(`http://localhost:8000/api/products/${productId}`, options)
+            .then(response => response.json())
+            .then(response => {
+
+                props.update(response.data)
+                setNewProductName("");
+                setNewProductPrice("");
+                setNewProductQuantity("");
+                setIsEditing(false)
+
+
+
+            })
+            .catch(err => console.error(err));
+    };
+
+
+
+    /**
+   * suppression d'un product par son ID
+   */
+    const handleDelete = (productsId: number) => {
+        console.log(productsId);
+
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+        fetch(`http://localhost:8000/api/products/${productsId}`, options)
+            .then(response => response.json())
+            .then(response => {
+
+                props.handleDeleteProduct(productsId)
+            })
+    };
+
+    return (
+        <tr>
+
+            <td>{props.product.id}</td>
+
+            {isEditing === false ? <td>{props.product.name}</td> : <td> <input type="name"
+                className="form-control"
+                id="nameControl"
+                onChange={(e) => setNewProductName(e.target.value)}
+                required></input></td>}
+            {isEditing === false ? <td>{props.product.price}</td> : <td><input type="price"
+                className="form-control"
+                id="priceControl"
+                onChange={(e) => setNewProductPrice(e.target.value)}
+                required></input></td>}
+            {isEditing === false ? <td>{props.product.quantity}</td> : <td> <input type="quantity"
+                className="form-control"
+                id="quantityControl"
+                onChange={(e) => setNewProductQuantity(e.target.value)}
+                required></input></td>}
+            <td>
+                <button type="button" className="btn btn-primary mx-2" onClick={() => {
+
+                    setIsEditing(true);
+                    setEditingProductId(props.product.id);
+                    setNewProductName(props.product!.name);
+                    setNewProductPrice(props.product!.price.toString());
+                    setNewProductQuantity(props.product!.quantity.toString());
+                }}
+                    id="update">
+                    modifier
+                </button>
+
+                <button type="button" className="btn btn-success mx-2" onClick={() => {
+                    handleUpdateProduct(editingProductId, newProductName, newProductPrice, newProductQuantity);
+
+                }}
+                    id="validate">
+                    valider
+                </button>
+
+                <button type="button" className="btn btn-danger" onClick={() => { handleDelete(props.product.id) }} id="delete">
+                    supprimer
+                </button>
+            </td>
+        </tr>
+    )
+
+
+
+}
+
+function setAllProducts(arg0: any) {
+    throw new Error("Function not implemented.");
+}
